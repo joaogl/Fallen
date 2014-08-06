ce79ae1fcd5d515b32e4c5f063597956ee10ef00
joaogl <joaoguerralourenco@hotmail.com>
2014-08-06 23:59:25 +0100

    More comments added.

--- src/com/webs/faragames/fallen/entity/light/SpotLight.java
+++ src/com/webs/faragames/fallen/entity/light/SpotLight.java
@@ -2,8 +2,26 @@ package com.webs.faragames.fallen.entity.light;
 
 import org.lwjgl.util.vector.Vector2f;
 
+/**
+ * Class for the Spot Light.
+ * 
+ * @author FARA Games
+ *
+ */
 public class SpotLight extends Light {
 
+	/**
+	 * Constructor to create the Light.
+	 * 
+	 * @param location
+	 *            : Vector2f with the light location.
+	 * @param red
+	 *            : float with the red value for the light color.
+	 * @param green
+	 *            : float with the green value for the light color.
+	 * @param blue
+	 *            : float with the blue value for the light color.
+	 */
 	public SpotLight(Vector2f location, float red, float green, float blue) {
 		super(location, red, green, blue);
 		this.type = 2;
@@ -11,6 +29,20 @@ public class SpotLight extends Light {
 		this.facing = 10;
 	}
 
+	/**
+	 * Constructor to create the Light.
+	 * 
+	 * @param location
+	 *            : Vector2f with the light location.
+	 * @param red
+	 *            : float with the red value for the light color.
+	 * @param green
+	 *            : float with the green value for the light color.
+	 * @param blue
+	 *            : float with the blue value for the light color.
+	 * @param inte
+	 *            : float with the light intensity.
+	 */
 	public SpotLight(Vector2f location, float red, float green, float blue, float inte) {
 		super(location, red, green, blue, inte);
 		this.type = 2;
@@ -18,6 +50,9 @@ public class SpotLight extends Light {
 		this.facing = 10;
 	}
 
+	/**
+	 * Method called once per second by the World Class.
+	 */
 	public void tick() {
 	}
 
